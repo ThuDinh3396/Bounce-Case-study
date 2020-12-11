68f4ca8850d8f8c46e7fc5dd22079e07fce5c5d1
Thu <thudth.ieit@ftu.edu.vn>
2020-12-11 21:26:04 +0700

    move

--- bounce.js
+++ bounce.js
@@ -1,10 +1,19 @@
-function Bounce(x, y, 10, 0, Math.PI * 2) {
+function Bounce(x, y, dx, radius) {
     this.x = x
     this.y = y
-
+    this.dx = dx
+    this.radius = radius
+}
+this.move = function () {
+    if (this.y + this.dy > canvas.height || this.y + this.dy < 0) {
+        this.dy = -this.dy;
+    }
+    if (this.x + this.dx > canvas.width || this.x + this.dx < 0) {
+        this.dx = -this.dx;
+    }
 }
 this.draw = function (color) {
     this.ctx.beginPath();
     this.ctx.fillStyle = color;
-    this.ctx.fillRect(this.x, this.y)
+    this.ctx.arc(this.x, this.y, this.dx, this.radius)
 }
