d6b75b5566cbbbc3c9181e4ae452e8da05a36b2a
Thu <thudth.ieit@ftu.edu.vn>
2020-12-11 21:09:36 +0700

    draw

--- bounce.js
+++ bounce.js
@@ -1,1 +1,10 @@
-function Bounce()
+function Bounce(x, y, 10, 0, Math.PI * 2) {
+    this.x = x
+    this.y = y
+
+}
+this.draw = function (color) {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(this.x, this.y)
+}
